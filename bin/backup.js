6075e50708e72442896164af585c34b3d9c29a32
Michael Novotny <manovotny@gmail.com>
2018-11-11 15:51:49 -0600

    Refactor.

--- bin/backup.js
+++ bin/backup.js
@@ -12,6 +12,61 @@ const throwError = (message) => {
     process.exit(1);
 };
 
+const getExcludes = (excludes) => {
+    if (excludes) {
+        return excludes.map((item) => `--exclude=${item}`).join(' ');
+    }
+
+    return [];
+};
+
+const generateRsyncCommand = ({destination, excludes, source}) =>
+    `rsync --compress --delete --links --progress --recursive --relative --stats --times --verbose --rsh=ssh ${excludes} "${source}" "${destination}"`;
+
+const run = ({destination, excludes, sources, verbose}) => {
+    const commands = [];
+
+    sources.forEach((source) => {
+        const command = generateRsyncCommand({destination, excludes, source});
+
+        commands.push({
+            task: () => execa.shell(command),
+            title: source
+        });
+    });
+
+    const tasks = new Listr(commands, {
+        renderer: verbose ? 'verbose' : 'default'
+    });
+
+    console.log(`Backing up...`);
+
+    tasks
+        .run()
+        .then(() => console.log('Backup complete! 🎉'))
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+const write = async ({destination, excludes, output, sources}) => {
+    const commands = ['clear'];
+
+    sources.forEach((source) => {
+        commands.push(
+            ...[
+                'echo ""',
+                `echo "Backing up: ${source}"`,
+                'echo ""',
+                generateRsyncCommand({destination, excludes, source})
+            ]
+        );
+    });
+
+    await writeFile(output, commands.join('\n'));
+    await execa('chmod', ['777', output]);
+};
+
 (async () => {
     const verbose = process.argv.includes('--verbose');
     const configPath = join(process.env.HOME, '.backup', 'config.json');
@@ -21,10 +76,8 @@ const throwError = (message) => {
         throwError('Configuration file required at `~/.backup/config.json`.');
     }
 
-    const {destination, exclude, sources, write} = await readJson(configPath);
-    const commands = [];
-
-    let excludes = '';
+    const {destination, exclude, output, sources} = await readJson(configPath);
+    const excludes = getExcludes(exclude);
 
     if (!destination) {
         throwError('Configuration file requires a `destination` property.');
@@ -34,55 +87,17 @@ const throwError = (message) => {
         throwError('Configuration file requires a `sources` property.');
     }
 
-    if (exclude) {
-        excludes = exclude.map((item) => `--exclude=${item}`).join(' ');
-    }
-
-    if (write) {
-        commands.push('clear');
-
-        sources.forEach((source) => {
-            commands.push(
-                ...[
-                    'echo ""',
-                    `echo "Backing up: ${source}"`,
-                    'echo ""',
-                    `rsync --compress --delete --links --progress --recursive --relative --stats --times --verbose --rsh=ssh ${excludes} "${source}" "${destination}"`
-                ]
-            );
-        });
-
-        await writeFile(write, commands.join('\n'));
-        await execa('chmod', ['777', write]);
+    if (output) {
+        await write({destination, excludes, output, sources});
     } else {
-        sources.forEach((source) => {
-            const command = `rsync --compress --delete --links --progress --recursive --relative --stats --times --verbose --rsh=ssh ${excludes} "${source}" "${destination}"`;
-
-            commands.push({
-                task: () => execa.shell(command),
-                title: source
-            });
-        });
-
-        const tasks = new Listr(commands, {
-            renderer: verbose ? 'verbose' : 'default'
-        });
-
-        console.log(`Backing up...`);
-
-        tasks
-            .run()
-            .then(() => console.log('Backup complete! 🎉'))
-            .catch((error) => {
-                console.error(error);
-            });
-
-        /*
-         * var backupProcesses = shell.exec('ps -ef | grep "node /usr/local/bin/backup" | grep -v grep  | wc -l', {silent: true}).output;
-         * if (parseInt(backupProcesses.trim()) > 1) {
-         *     console.log('The backup script is already running.');
-         *     shell.exit(0);
-         * }
-         */
+        run({destination, excludes, sources, verbose});
     }
+
+    /*
+     * var backupProcesses = shell.exec('ps -ef | grep "node /usr/local/bin/backup" | grep -v grep  | wc -l', {silent: true}).output;
+     * if (parseInt(backupProcesses.trim()) > 1) {
+     *     console.log('The backup script is already running.');
+     *     shell.exit(0);
+     * }
+     */
 })();
