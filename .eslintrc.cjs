2c5fe1eb8061780e37b717ed14907f404600309c
Michael Novotny <manovotny@gmail.com>
2022-03-20 21:17:11 -0500

    Updates to Node 16 and removes yarn. (#4)

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: 'get-off-my-lawn',
+    rules: {
+        'no-console': 0,
+    },
+};
--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['get-off-my-lawn-prettier'],
-    rules: {
-        complexity: 0,
-        'no-console': 0
-    }
-};
